ca569a802b0d25826df33aa75af0bbe52cac4a7c
FikalAhmad <fikaalm26@gmail.com>
2025-02-28 21:17:50 +0700

    fix: fix route authorization

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,3 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-import AppSidebar from './components/AppSidebar.vue'
-import AppTopbar from './components/AppTopbar.vue'
-
-const isSidebarVisible = ref(true)
-
-const toggleProfile = () => {
-  isSidebarVisible.value = !isSidebarVisible.value
-}
-</script>
-
 <template>
-  <div class="flex">
-    <div class="">
-      <AppSidebar :visible="isSidebarVisible" />
-    </div>
-    <div class="w-full">
-      <AppTopbar @toggle-sidebar="toggleProfile" />
-      <RouterView />
-    </div>
-  </div>
+  <RouterView />
 </template>
-
-<style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,48 +1,87 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import LandingPage from '@/views/LandingPage.vue'
+import DefaultLayout from '@/layouts/DefaultLayout.vue'
+import AuthLayout from '@/layouts/AuthLayout.vue'
+import { useAuthStore } from '@/store/auth'
 
 const routes = [
-  { path: '/', name: 'homepage', component: LandingPage },
-  { path: '/login', name: 'login', component: () => import('@/views/LoginPage.vue') },
-  { path: '/analytics', name: 'analytics', component: () => import('@/views/AnalyticsPage.vue') },
+  {
+    path: '/',
+    component: DefaultLayout,
+    children: [
+      { path: 'dashboard', name: 'homepage', component: LandingPage, meta: { requiresAuth: true } },
+      {
+        path: 'analytics',
+        name: 'analytics',
+        component: () => import('@/views/AnalyticsPage.vue'),
+        meta: { requiresAuth: true },
+      },
 
-  {
-    path: '/user',
-    name: 'user-management',
-    component: () => import('@/views/manage-user/UserList.vue'),
-  },
-  {
-    path: '/user/create',
-    name: 'create-user',
-    component: () => import('@/views/manage-user/CreateUser.vue'),
-  },
-  {
-    path: '/user/edit:id',
-    name: 'edit-user',
-    component: () => import('@/views/manage-user/EditUser.vue'),
-  },
+      {
+        path: 'user',
+        name: 'user-management',
+        component: () => import('@/views/manage-user/UserList.vue'),
+        meta: { requiresAuth: true },
+      },
+      {
+        path: 'user/create',
+        name: 'create-user',
+        component: () => import('@/views/manage-user/CreateUser.vue'),
+        meta: { requiresAuth: true },
+      },
+      {
+        path: 'user/edit/:id',
+        name: 'edit-user',
+        component: () => import('@/views/manage-user/EditUser.vue'),
+        meta: { requiresAuth: true },
+      },
 
-  {
-    path: '/product',
-    name: 'product-management',
-    component: () => import('@/views/manage-product/ProductList.vue'),
+      {
+        path: 'product',
+        name: 'product-management',
+        component: () => import('@/views/manage-product/ProductList.vue'),
+        meta: { requiresAuth: true },
+      },
+      {
+        path: 'order',
+        name: 'order-management',
+        component: () => import('@/views/manage-order/OrderList.vue'),
+        meta: { requiresAuth: true },
+      },
+      {
+        path: 'payment',
+        name: 'payment-management',
+        component: () => import('@/views/manage-payment/PaymentList.vue'),
+        meta: { requiresAuth: true },
+      },
+    ],
   },
   {
-    path: '/order',
-    name: 'order-management',
-    component: () => import('@/views/manage-order/OrderList.vue'),
-  },
-  {
-    path: '/payment',
-    name: 'payment-management',
-    component: () => import('@/views/manage-payment/PaymentList.vue'),
+    path: '/login',
+    component: AuthLayout,
+    children: [
+      {
+        path: '',
+        name: 'Login',
+        component: () => import('@/views/LoginPage.vue'),
+      },
+    ],
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+  if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
