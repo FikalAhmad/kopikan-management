6c4bd0ceacec1f0497accca99e2e0e3f9c516383
FikalAhmad <fikaalm26@gmail.com>
2025-02-28 21:17:24 +0700

    add pinia and persist lib

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
+import piniaPersistedState from 'pinia-plugin-persistedstate'
 
-createApp(App).use(router).mount('#app')
+const pinia = createPinia()
+pinia.use(piniaPersistedState)
+
+const app = createApp(App)
+app.use(pinia)
+app.use(router)
+app.mount('#app')
